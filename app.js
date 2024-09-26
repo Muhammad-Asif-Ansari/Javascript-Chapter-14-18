73e14182ae6abb69cb65ff4580695f78bb512f81
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2024-09-26 23:27:56 +0500

    Assignment JavaScript
    Assignment JavaScript

---
+++ app.js
@@ -0,0 +1,33 @@
+// ASSIGNMENT 05:
+// Chapter 14-18;
+// QUESTION NO 01:
+// var studentName = [""]
+// QUESTION NO 02:
+// var studentName = ["ALI","IMRAN","NAWAZ","TAHIR"];
+// QUESTION NO 03:
+// var number = [2,5,8,6];
+// QUESTION NO 04:
+// var boolean = [true,false];
+// QUESTION NO 05:
+// var mixed = ["Hafeez",5,true];
+// QUESTION NO 06:
+// var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil", "PhD"];
+// document.write("<b>Qualifacations:</b>"+ "" + "<br>")
+// document.write("1." + qualifications[0] +"<br>");
+// document.write("2." + qualifications[1] +"<br>");
+// document.write("3." + qualifications[2] +"<br>");
+// document.write("4." + qualifications[3] +"<br>");
+// document.write("5." + qualifications[4] +"<br>");
+// document.write("6." + qualifications[5] +"<br>");
+// document.write("7." + qualifications[6] +"<br>");
+// document.write("8." + qualifications[7] +"<br>");
+
+// QUESTION N0 07:
+// var studentsName = ["Jamal","Kamal","Hilal"];
+// var studentScore = [320,230,480];
+// var student1 = studentScore[0] / 500 * 100;
+// var student2 = studentScore[1] / 500 * 100;
+// var student3 = studentScore[2] / 500 * 100;
+// document.write("Score of " + studentsName[0] + " is 320" + "<br>" + "Percentage is " + student1 + "% " +"<br>");
+// document.write("Score of " + studentsName[1] + " is 230" + "<br>" + "Percentage is " + student2 + "% " +"<br>");
+// document.write("Score of " + studentsName[2] + " is 480" + "<br>" + "Percentage is " + student3 + "% " +"<br>");
